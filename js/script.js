40eeb2e338699b9908a9c575d731a73930c33456
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-05 14:24:00 +0100

    RELEASE new repo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// alert() 
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito
+
+// nome
+// cognome
+// colore preferito
+//collegamento a #container
+//inserimento valori (nomecognomecolorepreferito)
+
+let userFirstName = prompt("Come ti chiami?");
+let userLastName = prompt("Qual è il tuo cognome?");
+let userColor = prompt("...e il tuo colore preferito?");
+console.log(userFirstName)
+console.log(userLastName)
+console.log(userColor)
+document.getElementById('container').innerHTML = `${userFirstName} ${userLastName} ${userColor}`
